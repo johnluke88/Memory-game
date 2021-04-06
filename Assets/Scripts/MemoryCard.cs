cb29808dcb92975161a57947de1740a4927200f3
johnluke88 <gianluca.pirone9@gmail.com>
2021-04-06 12:43:02 +0200

    Initial commit

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    [SerializeField] private GameObject cardBack;
+    [SerializeField] private Sprite image;
+    [SerializeField] private SceneController controller;
+    private int _id;
+
+    public int id {
+        get { return _id; }
+    }
+
+    public void SetCard(int id, Sprite image) {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnMouseDown()
+    {
+        if (cardBack.activeSelf) {
+            cardBack.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] private MemoryCard originalCard;
+    [SerializeField] private Sprite[] images;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int id = Random.Range(0, images.Length);
+        originalCard.SetCard(id, images[id]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
